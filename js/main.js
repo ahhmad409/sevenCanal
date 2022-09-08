c5db14abba269701f6b0290e8d075e26e8d7ea4f
Ahmad Hameed <ahhmad409@gmail.com>
2022-09-08 10:29:25 +0500

    all issues/bugs resolved
    flickering navbar dropdown resolved. go to homepage on logo click done, go on top of page on footer 7 canal click done, etc... all qa issues resolved

--- js/main.js
+++ js/main.js
@@ -29,106 +29,106 @@
   // });
 
   // Dropdown on mouse hover
-  $(document).ready(function () {
-    function toggleNavbarMethod() {
-      if ($(window).width() > 992) {
-        $(".navbar .dropdown")
-          .on("mouseover", function () {
-            $(".dropdown-toggle", this).trigger("click");
-          })
-          .on("mouseout", function () {
-            $(".dropdown-toggle", this).trigger("click").blur();
-          });
-      } else {
-        $(".navbar .dropdown").off("mouseover").off("mouseout");
-      }
-    }
-    toggleNavbarMethod();
-    $(window).resize(toggleNavbarMethod);
-  });
+  // $(document).ready(function () {
+  //   function toggleNavbarMethod() {
+  //     if ($(window).width() > 992) {
+  //       $(".navbar .dropdown")
+  //         .on("mouseover", function () {
+  //           $(".dropdown-toggle", this).trigger("click");
+  //         })
+  //         .on("mouseout", function () {
+  //           $(".dropdown-toggle", this).trigger("click").blur();
+  //         });
+  //     } else {
+  //       $(".navbar .dropdown").off("mouseover").off("mouseout");
+  //     }
+  //   }
+  //   toggleNavbarMethod();
+  //   $(window).resize(toggleNavbarMethod);
+  // });
 
   // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 2000,
-  });
+  // $('[data-toggle="counter-up"]').counterUp({
+  //   delay: 10,
+  //   time: 2000,
+  // });
 
   // Modal Video
-  $(document).ready(function () {
-    var $videoSrc;
-    $(".btn-play").click(function () {
-      $videoSrc = $(this).data("src");
-    });
-    console.log($videoSrc);
+  // $(document).ready(function () {
+  //   var $videoSrc;
+  //   $(".btn-play").click(function () {
+  //     $videoSrc = $(this).data("src");
+  //   });
+  //   console.log($videoSrc);
 
-    $("#videoModal").on("shown.bs.modal", function (e) {
-      $("#video").attr(
-        "src",
-        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
-      );
-    });
+  //   $("#videoModal").on("shown.bs.modal", function (e) {
+  //     $("#video").attr(
+  //       "src",
+  //       $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+  //     );
+  //   });
 
-    $("#videoModal").on("hide.bs.modal", function (e) {
-      $("#video").attr("src", $videoSrc);
-    });
-  });
+  //   $("#videoModal").on("hide.bs.modal", function (e) {
+  //     $("#video").attr("src", $videoSrc);
+  //   });
+  // });
 
   // Testimonial Slider
-  $(".testimonial-slider").slick({
-    infinite: true,
-    autoplay: true,
-    arrows: false,
-    dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".testimonial-slider-nav",
-  });
-  $(".testimonial-slider-nav").slick({
-    arrows: false,
-    dots: false,
-    focusOnSelect: true,
-    centerMode: true,
-    centerPadding: "22px",
-    slidesToShow: 3,
-    asNavFor: ".testimonial-slider",
-  });
-  $(".testimonial .slider-nav").css({ position: "relative", height: "160px" });
+  // $(".testimonial-slider").slick({
+  //   infinite: true,
+  //   autoplay: true,
+  //   arrows: false,
+  //   dots: false,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   asNavFor: ".testimonial-slider-nav",
+  // });
+  // $(".testimonial-slider-nav").slick({
+  //   arrows: false,
+  //   dots: false,
+  //   focusOnSelect: true,
+  //   centerMode: true,
+  //   centerPadding: "22px",
+  //   slidesToShow: 3,
+  //   asNavFor: ".testimonial-slider",
+  // });
+  // $(".testimonial .slider-nav").css({ position: "relative", height: "160px" });
 
   // Blogs carousel
-  $(".related-slider").owlCarousel({
-    autoplay: true,
-    dots: false,
-    loop: true,
-    nav: true,
-    navText: [
-      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-    ],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      576: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-    },
-  });
+  // $(".related-slider").owlCarousel({
+  //   autoplay: true,
+  //   dots: false,
+  //   loop: true,
+  //   nav: true,
+  //   navText: [
+  //     '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+  //     '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+  //   ],
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     576: {
+  //       items: 1,
+  //     },
+  //     768: {
+  //       items: 2,
+  //     },
+  //   },
+  // });
 
   // Portfolio isotope and filter
-  var portfolioIsotope = $(".portfolio-container").isotope({
-    itemSelector: ".portfolio-item",
-    layoutMode: "fitRows",
-  });
+  // var portfolioIsotope = $(".portfolio-container").isotope({
+  //   itemSelector: ".portfolio-item",
+  //   layoutMode: "fitRows",
+  // });
 
-  $("#portfolio-flters li").on("click", function () {
-    $("#portfolio-flters li").removeClass("filter-active");
-    $(this).addClass("filter-active");
+  // $("#portfolio-flters li").on("click", function () {
+  //   $("#portfolio-flters li").removeClass("filter-active");
+  //   $(this).addClass("filter-active");
 
-    portfolioIsotope.isotope({ filter: $(this).data("filter") });
-  });
+  //   portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  // });
 })(jQuery);
 
 // const submitNewsletterBtn = document.getElementById("submit-newsletter-btn");
@@ -162,7 +162,7 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
     a.classList.add("btn", "btn-primary");
     a.setAttribute("id", "downloadBtn");
     a.setAttribute("type", "button");
-    a.setAttribute("download", "download");
+    // a.setAttribute("download", "download");
     a.setAttribute("href", "#");
     document.querySelector(".modal-footer").replaceChild(a, downloadButton);
 
@@ -261,3 +261,13 @@ const allLinks = document.querySelectorAll(".downloadLink").forEach((link) => {
 //       console.log(err);
 //     });
 // });
+
+// https://www.tutorialrepublic.com/faq/how-to-open-bootstrap-dropdown-menu-on-hover-rather-than-click.php#:~:text=Answer%3A%20Use%20the%20jQuery%20hover,using%20the%20CSS%20and%20jQuery.
+$(document).ready(function () {
+  $(".dropdown").hover(function () {
+    var dropdownMenu = $(this).children(".dropdown-menu");
+    if (dropdownMenu.is(":visible")) {
+      dropdownMenu.parent().toggleClass("open");
+    }
+  });
+});
